8727ecd7ecbe3507f6f2d6633a3e6b60480b8341
Jiri Peska <jpeska@redhat.com>
2023-12-01 14:45:06 +0100

    IK is working

--- kinematic-calculation/IK-arm.py
+++ kinematic-calculation/IK-arm.py
@@ -1,5 +1,7 @@
 import math
+import numpy as np
 from matplotlib import pyplot as plt
+from plot import *
 
 # https://technology.cpm.org/general/3dgraph/
 
@@ -73,10 +75,10 @@ class Kinematics:
         shoulder_rad = math.radians(shoulder_angle) # shoulder
         knee_rad = math.radians(knee_angle) # elbow
 
-        P0 = leg.leg_placement_coords
+        P0 = Coords(0, 0, 10)
 
         # Pohyb kloubuu v ramci vertikalni osy xy
-        P1 = Coords(P0.x + d_base + coxa_len * math.cos(shoulder_rad), P0.y + coxa_len * math.sin(shoulder_rad), P0.z)
+        P1 = Coords(P0.x + coxa_len * math.cos(shoulder_rad), P0.y + coxa_len * math.sin(shoulder_rad), P0.z)
 
         # print(round(P1[0], 3), round(P1[1], 3))
         P2 = Coords(P1.x + femur_len * math.cos(shoulder_rad + knee_rad), P1.y + femur_len * math.sin(shoulder_rad + knee_rad), P1.z)
@@ -86,18 +88,6 @@ class Kinematics:
         P3 = (P0.x + P2.x * math.cos(base_rad), P2.y, P2.x * math.sin(base_rad))
         print("FK Output coords:", round(P3[0], 6), round(P3[1], 6), round(P3[2], 6))
 
-        # plt.rcParams["figure.figsize"] = [7.50, 3.50]
-        # plt.rcParams["figure.autolayout"] = True
-        # fig = plt.figure()
-        # ax = fig.add_subplot(projection="3d")
-        # ax.set_xlabel('x')
-        # ax.set_ylabel('y')
-        # ax.set_zlabel('z')
-        # x, y, z = [P0[0]+d_base, P0[0], P1[0], P2[0], P3[0]], [P0[1],P0[1], P1[1], P2[1], P3[1]], [P0[2], P0[2], P1[2], P2[2], P3[2]]
-        # ax.scatter(x, y, z, c='red', s=100)
-        # ax.plot(x, y, z, color='black')
-        # plt.show()
-
         return (round(P3.x, 3), round(P3.y, 3), round(P3.z, 3))
 
     def inverse_kinematics(self, leg: HexapodLeg, target: Coords):
@@ -105,78 +95,6 @@ class Kinematics:
 
     def calc_2D_ik(self, femur_len, tibia_len, tx, ty):
         pass
-
-    def calc_3D_ik3(self, femur_len, tibia_len, coxa_len, tx, ty, tz):
-        # TODO: Brat v uvahu pocatecni bod pro vypocet (napr P0 jak u FK, kde y=10)
-        # TODO: Zakomponovat d_base
-        base = 0
-        shoulder = 0
-        elbow = 0
-
-        l0 = math.sqrt(tx**2 + tz**2)
-        # tz += 2
-
-        # top view (XZ plane)
-        base_rad = math.atan2(tz, tx)
-        base = round(math.degrees(base_rad))
-        print("base", base) #OK
-
-        # side view (XZ-Y plane)
-        # shoulder_rad = math.acos((l0**2 + femur_len**2 - tibia_len**2)/(2 * l0 * femur_len))
-        # elbow_rad = math.acos((tibia_len**2 + femur_len**2 - l0**2)/(2 * femur_len * tibia_len))
-
-        tx -= coxa_len
-        # ty += 10
-
-        # l0 = math.sqrt(tx**2 + ty**2)
-        elbow_rad = math.acos((tx**2 + ty**2 - femur_len**2 - tibia_len**2) / (2*femur_len*tibia_len))
-        elbow = math.degrees(elbow_rad)
-        shoulder_rad = math.atan2(tx, ty) - math.asin((tibia_len * math.sin(elbow)) / (math.sqrt(tx**2 + ty**2)))
-        # shoulder_rad = math.atan2(tx, ty) - math.atan2(tibia_len*math.sin(elbow), femur_len + tibia_len*math.cos(elbow))
-        # shoulder_rad = (-ty * tibia_len * math.sin(elbow) + tx * (femur_len + tibia_len * math.cos(elbow))) / (tx*tibia_len* math.sin(elbow) + ty * (femur_len + tibia_len * math.cos(elbow)))
-        shoulder = math.degrees(shoulder_rad)
-
-        print("shoulder", shoulder)
-        print("elbow", elbow)
-
-        return (base, shoulder, elbow)
-        # return (base, shoulder_rad, elbow_rad)
-        # return (base_rad, shoulder_rad, elbow_rad)
-
-
-
-    def calc_3D_ik2(self, femur_len, tibia_len, coxa_len, tx, ty, tz):
-        # top view (XZ plane)
-        base_rad = math.atan2(tz, tx)
-        base = round(math.degrees(base_rad))
-        # print("base", base) #OK
-
-        # Distance between the Coxa and Ground Contact
-        height_offset = math.sqrt(tx**2 + ty**2)
-
-        # ikSW - Length between Femur axis and Tibia (prepona 'c' mezi femurem a tibia)
-        ikSW = math.sqrt((height_offset - coxa_len)**2 + tz**2)
-
-        # ikRadiansFemurTibiaGround - Angle between Femur and Tibia line and the ground in radians
-        ikRadiansFemurTibiaGround = math.atan2(height_offset - coxa_len, tz)
-
-        # ikRadiansFemurTibia - Angle of the line Femur and Tibia with respect to the Femur in radians
-        ikRadiansFemurTibia = math.acos(((femur_len**2 - tibia_len**2) + ikSW**2) / (2 * femur_len * ikSW))
-
-        # ikCoxaAngle in degrees
-        initCoxaAngle = 0
-        coxaAngle = math.atan2(ty, tx) * 180 / math.pi +initCoxaAngle
-
-        # ikFemurAngle in degrees
-        femurAngle = - (ikRadiansFemurTibiaGround + ikRadiansFemurTibia ) * 180 / math.pi  + 90
-
-        # ikTibiaAngle in degrees
-        tibiaAngle = - (90 -(((math.acos((femur_len**2 + tibia_len**2 - ikSW**2 ) / ( 2 * femur_len * tibia_len ) ) ) * 180 ) / math.pi ) )
-
-        # return (base, shoulder, elbow)
-        return (coxaAngle, femurAngle, tibiaAngle)
-        # return (base, shoulder_rad, elbow_rad)
-        # return (base_rad, shoulder_rad, elbow_rad)
 
     def ik(self, leg: HexapodLeg, target: Coords):
         tx = target.x
@@ -187,7 +105,7 @@ class Kinematics:
         a2 = leg.femur_len
         a3 = leg.tibia_len
 
-        theta1 = math.atan2(ty, tx)  # [1]
+        theta1 = math.degrees(math.atan2(ty, tx))  # [1]
         r1 = math.sqrt(tx**2 + ty**2) - leg.body_coxa_distance # [2]
 
         if r1 > a2 + a3:
@@ -203,88 +121,8 @@ class Kinematics:
         theta3 = math.degrees(phi3)  # [9]
         return (theta1, theta2, theta3)
 
-# Test
-# calc_3D_fk((0, 10), 5, 3, d_base=2, a0=45, a1=10, a3=180)
-
-
-# def calc_3D_ik(femur_len, tibia_len, tx, ty, tz):
-#     # TODO: Zakomponovat d_base
-#     # if (femur_len + tibia_len) >= tx or (femur_len + tibia_len) >= ty or (femur_len + tibia_len) >= tz:
-#         # print("Invalid values")
-
-#     r = math.sqrt(tx**2 + ty**2 + tz**2)
-#     base = math.atan2(ty,tx)
-#     acos_value = (r**2 - femur_len**2 - tibia_len**2) / (2 * femur_len * tibia_len)
-#     # print(acos_value)
-#     elbow = -math.acos(acos_value)
-#     shoulder = math.asin(tz/r) + math.atan2((tibia_len * math.sin(elbow)), (femur_len + (tibia_len * math.cos(elbow))))
-
-#     print("tx, ty, tz", tx, ty, tz)
-#     print("r", r)
-#     print("base", math.degrees(base))
-#     print("elbow", math.degrees(elbow))
-#     print("shoulder", math.degrees(shoulder))
-#     print()
-
-
-
-# ============= TEST ==============================
-# Test:
-# calc_3D_ik(femur_len, tibia_len, 10, 0, 0)
-# calc_3D_ik(femur_len, tibia_len, 0, 10, 0)
-# calc_3D_ik(femur_len, tibia_len, 0, 0, 10)
-
-# calc_3D_ik(femur_len, tibia_len, 0, 5, 5)
-# calc_3D_ik(femur_len, tibia_len, 5, 0, 5)
-# calc_3D_ik(femur_len, tibia_len, 5, 5, 0)
-# calc_3D_ik(femur_len, tibia_len, 5, 5, 5)
-
-# Draw a line - Path of the hand from x=1 to x=10
-# for x in range(-5, 5):
-    # calc_3D_ik(femur_len, tibia_len, x, 5, 0)
-
-base_len = 2  # 2 cm
-femur_len = 6.5
-tibia_len = 12
-
-base = 20
-shoulder = 20
-elbow = -50
-
-print("base", base)
-print("shoulder", shoulder)
-print("elbow", elbow)
-print()
-
-# xyz = calc_3D_fk((0,0), femur_len, tibia_len, base_len, shoulder, elbow, base)
-# print()
-# # angles = calc_3D_ik2(femur_len, tibia_len, 10, 0, 10)
-# angles = calc_3D_ik2(femur_len, tibia_len, base_len, xyz[0], xyz[1], xyz[2])
-# print("coxa", angles[0], "femur", angles[1], "tibia", angles[2])
-# print()
-# xyz2 = calc_3D_fk((0,0), femur_len, tibia_len, base_len, angles[0], angles[1], angles[2])
-
-
-### samostatny test
-print("Samsotatne testy")
-print()
-# angles = calc_3D_ik2(femur_len, tibia_len, base_len, 20.5, 0, 0)
-# print("coxa", angles[0], "femur", angles[1], "tibia", angles[2])
-
-# calc_3D_fk((0,0,0), femur_len, tibia_len, base_len, 0, 0, 0)
-# calc_3D_fk((0,10,0), femur_len, tibia_len, base_len, 160, 90, 0) # Klasicke postaveni nohy (160,90,0)
-# calc_3D_fk((0,10,0), femur_len, tibia_len, base_len, 90, 190, 0)
-# angles = calc_3D_ik2(femur_len, tibia_len, base_len, -8.212244, 0.946819, 0)
-# print("coxa", angles[0], "femur", angles[1], "tibia", angles[2])
-
-
-# TODO: Udelat testy - hlavne pro xyz ze
-# (20.5, 0, 0) = (0, 0, 0) # natazena noha, vychozi pozice
-# (0, 18.5, 0) = (90, 0, 0) # Natazena noha, ale smerem nahoru
-# (0, 0, 18.5) = (0, 0, 90) # Natazena noha, ale smerem nahoru
 
 leg = HexapodLeg(0, None, 2, 10, 6.5, 12)
-
 kinematics = Kinematics()
 
 init_x = 20.5
@@ -316,4 +154,25 @@ cmd = ""
 
 for x in range(10, 20):
     print(kinematics.ik(leg, Coords(x, 0, 10)))
-print(kinematics.ik(leg, Coords(15, 0, 10)))
+# for y in range(5, 10):
+#     print(kinematics.ik(leg, Coords(5, y, 10)))
+# for z in range(10, 20):
+#     print(kinematics.ik(leg, Coords(0, 0, z)))
+
+
+plotter = Plotter()
+print("kruh")
+for x in np.arange(10, 20, 0.5):
+    plotter.add_point((10, 5*math.cos(x), 5*math.sin(x)))
+    # plotter.add_point(kinematics.ik(leg, Coords(10, 5*math.cos(x), 5*math.sin(x))))
+
+plotter.plot_points()
+plotter.empty_points()
+
+# print("cara")
+# for x in np.arange(10, 17, 0.5):
+#     print(kinematics.ik(leg, Coords(x, 0, 10)))
+#     plotter.add_point(kinematics.ik(leg, Coords(x, 0, 10)))
+# plotter.plot_points()
+
+
