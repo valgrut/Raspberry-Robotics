27ec415529c669a354b0b3c1d87b12f42aa753c5
valgrut <naruto987@seznam.cz>
2023-12-11 01:02:04 +0100

    Moved kinematics doc

--- rpi-hexapod/src/m_hexapod.py
+++ rpi-hexapod/src/m_hexapod.py
@@ -1,6 +1,13 @@
 from coords import Coords
 from time import sleep
 from adafruit_servokit import ServoKit
+# Other imports are at the bottom of the file
+
+
+BASE_SERVO_ID = 0
+SHOULDER_SERVO_ID = 1
+ELBOW_SERVO_ID = 2
+
 
 class Hexapod:
     def __init__(self):
@@ -19,6 +26,99 @@ class Hexapod:
     def walk(self):
         pass
 
+    def interactive_effector_control(self, leg_id: int):
+        # xyz pozice na relativne normalni pozici nohou:
+        init_x = 7
+        init_y = 1
+        init_z = 8
+
+        angles = None
+        old_angles = None
+
+        increment = 1
+        cmd = ""
+        while cmd != "e":
+            cmd = input()
+            if cmd == "w":
+                init_x -= increment
+            elif cmd == "s":
+                init_x += increment
+
+            if cmd == "a":
+                init_y += increment
+            elif cmd == "d":
+                init_y -= increment
+
+            if cmd == "r":
+                init_z += increment
+            elif cmd == "f":
+                init_z -= increment
+
+            print("new x", init_z)
+            print("new y", init_y)
+            print("new z", init_x)
+
+            try:
+                old_angles = angles
+
+                angles = self.kinematics.inverse_kinematics(self.legs[leg_id], Coords(init_x, init_y, init_z))
+                print(angles)
+
+                # kit.servo[0].angle = map_range(angles[2], -90, 90, 0, 180)
+                # kit.servo[1].angle = map_range(angles[1], -90, 90, 0, 180)
+                # kit.servo[2].angle = map_range(angles[0], -90, 90, 0, 180)
+
+                self.legs[leg_id].set_angle(BASE_SERVO_ID, angles[2])
+                self.legs[leg_id].set_angle(SHOULDER_SERVO_ID, angles[1])
+                self.legs[leg_id].set_angle(ELBOW_SERVO_ID, angles[0])
+
+            except:
+                print("Invalid angles")
+                angles = old_angles
+
+
+    def interactive_angle_control(self, leg_id: int):
+        # init angles of the arm
+        init_base_angle = 100
+        init_shoulder_angle = 50
+        init_elbow_angle = 100
+        increment = 10
+
+        # TODO: zjistit, na jakych pozcich xyz se effector pohybuje
+
+        cmd = ""
+        while cmd != "e":
+            cmd = input()
+            if cmd == "a":
+                init_base_angle = init_base_angle + increment if init_base_angle + increment < 180 else 180
+            elif cmd == "d":
+                init_base_angle = init_base_angle - increment if init_base_angle - increment > 0 else 0
+
+            if cmd == "w":
+                init_shoulder_angle = init_shoulder_angle + increment if init_shoulder_angle + increment < 180 else 180
+            elif cmd == "s":
+                init_shoulder_angle = init_shoulder_angle - increment if init_shoulder_angle - increment > 0 else 0
+
+            if cmd == "r":
+                init_elbow_angle = init_elbow_angle + increment if init_elbow_angle + increment < 180 else 180
+            elif cmd == "f":
+                init_elbow_angle = init_elbow_angle - increment if init_elbow_angle - increment > 0 else 0
+
+            print("angle base", init_base_angle)
+            print("angle shoulder", init_shoulder_angle)
+            print("angle elbow", init_elbow_angle)
+
+            self.kit.servo[2].angle = init_base_angle
+            self.kit.servo[1].angle = init_shoulder_angle
+            self.kit.servo[0].angle = init_elbow_angle
+
+            # self.set_angle(self.BASE_SERVO_ID, init_elbow_angle)
+            # self.set_angle(self.SHOULDER_SERVO_ID, init_shoulder_angle)
+            # self.set_angle(self.ELBOW_SERVO_ID, init_base_angle)
+
+            print(self.kinematics.forward_kinematics(self.legs[leg_id], init_base_angle, init_shoulder_angle, init_elbow_angle))
+
+
 
 class HexapodLeg:
     def __init__(self, hexapod: Hexapod, leg_idx, coxa_len, femur_len, tibia_len):
@@ -36,11 +136,6 @@ class HexapodLeg:
 
         self.kinematics = self.hexapod.kinematics
         self.kit = self.hexapod.kit
-
-        # Konstants
-        self.BASE_SERVO_ID = 3 * self.leg_idx + 0
-        self.SHOULDER_SERVO_ID = 3 * self.leg_idx + 1
-        self.ELBOW_SERVO_ID = 3 * self.leg_idx + 2
 
 
     def move_to_point(self, end_point: Coords):
@@ -71,7 +166,6 @@ class HexapodLeg:
 
             #sleep(2)
 
-
     def draw_y_line(self):
         # Move leg back and forth along Y axis
         for y in range(5, 15):
@@ -84,100 +178,12 @@ class HexapodLeg:
             print(self.kinematics.inverse_kinematics(self, Coords(10, 0, z)))
 
 
-
     def set_angle(self, servo_id: int, angle):
-        self.kit.servo[servo_id].angle = angle
+        assert(servo_id < 3)
+        assert(servo_id >= 0)
+        print("Angle", angle)
+        self.kit.servo[3 * self.leg_idx + servo_id].angle = angle
 
-    def interactive_effector_control(self, leg_id: int):
-        # xyz pozice na relativne normalni pozici nohou:
-        init_x = 7 #75
-        init_y = 1 #120
-        init_z = 8 #100
-
-        angles = None
-        old_angles = None
-
-        increment = 1 #10
-        cmd = ""
-        while cmd != "e":
-            cmd = input()
-            if cmd == "w":
-                init_x -= increment
-            elif cmd == "s":
-                init_x += increment
-
-            if cmd == "a":
-                init_y += increment
-            elif cmd == "d":
-                init_y -= increment
-
-            if cmd == "r":
-                init_z += increment
-            elif cmd == "f":
-                init_z -= increment
-
-            print("new x", init_z)
-            print("new y", init_y)
-            print("new z", init_x)
-
-            try:
-                old_angles = angles
-                angles = self.kinematics.inverse_kinematics(self, Coords(init_x, init_y, init_z))
-
-                # kit.servo[0].angle = map_range(angles[2], -90, 90, 0, 180)
-                # kit.servo[1].angle = map_range(angles[1], -90, 90, 0, 180)
-                # kit.servo[2].angle = map_range(angles[0], -90, 90, 0, 180)
-
-                self.set_angle(self.BASE_SERVO_ID, angles[2])
-                self.set_angle(self.SHOULDER_SERVO_ID, angles[1])
-                self.set_angle(self.ELBOW_SERVO_ID, angles[0])
-
-                print(self.kinematics.inverse_kinematics(self, Coords(init_x, init_y, init_z)))
-            except:
-                print("Invalid angles")
-                angles = old_angles
-
-
-    def interactive_angle_control(self, leg_id: int):
-        # Angles:
-        # TODO: Chtelo by to Forward Kinematic !!! a zjistit, na
-        # jakych pozcich xyz se effector pohybuje
-        init_base_angle = 70
-        init_shoulder_angle = 120
-        init_elbow_angle = 100
-        increment = 10
-
-        cmd = ""
-        while cmd != "e":
-            cmd = input()
-            if cmd == "w":
-                init_base_angle = init_base_angle + increment if init_base_angle + increment < 180 else 180
-            elif cmd == "s":
-                init_base_angle = init_base_angle - increment if init_base_angle - increment > 0 else 0
-
-            if cmd == "a":
-                init_shoulder_angle = init_shoulder_angle + increment if init_shoulder_angle + increment < 180 else 180
-            elif cmd == "d":
-                init_shoulder_angle = init_shoulder_angle - increment if init_shoulder_angle - increment > 0 else 0
-
-            if cmd == "r":
-                init_elbow_angle = init_elbow_angle + increment if init_elbow_angle + increment < 180 else 180
-            elif cmd == "f":
-                init_elbow_angle = init_elbow_angle - increment if init_elbow_angle - increment > 0 else 0
-
-            print("angle x", init_base_angle)
-            print("angle y", init_shoulder_angle)
-            print("angle z", init_elbow_angle)
-
-            self.kit.servo[1].angle = init_base_angle
-            self.kit.servo[0].angle = init_shoulder_angle
-            self.kit.servo[2].angle = init_elbow_angle
-
-            # self.set_angle(self.BASE_SERVO_ID, init_elbow_angle)
-            # self.set_angle(self.SHOULDER_SERVO_ID, init_shoulder_angle)
-            # self.set_angle(self.ELBOW_SERVO_ID, init_base_angle)
-
-            print(self.kinematics.forward_kinematics(self, init_base_angle, init_shoulder_angle, init_elbow_angle))
 
 # Prevent Circular import error
 from m_kinematics import Kinematics
--- rpi-hexapod/src/main.py
+++ rpi-hexapod/src/main.py
@@ -13,13 +13,13 @@ kinematics = Kinematics()
 kit = ServoKit(channels=8)
 
 hexapod = Hexapod()
-leg = HexapodLeg(hexapod, 0, 5, 6.5, 12)
+leg = HexapodLeg(hexapod, 0, 5.5, 6.4, 12)
 
 
 # Defaultni uhly, pri kterych je noha uplne natazena
-#a0 = 100 # Base:  100 = Natazena base presne uprostred
-#a1 = 50  # Rameno: 50 = natazene rameno
-#a2 = 100 # Loket: 100 = presne 45stupnu dolu od stredni casti nohy
+#[2] = 100 # Base:  100 = Natazena base presne uprostred
+#[1] = 50  # Rameno: 50 = natazene rameno
+#[0] = 100 # Loket: 100 = presne 45stupnu dolu od stredni casti nohy
 # TODO: V IK proste pro (0,0,0) odecist/pricist takove hodnoty, aby natazena noha byla a0=100, a1=50, ?a2=100?
 
 
@@ -32,3 +32,13 @@ leg.draw_y_line()
 leg.draw_x_line()
 
 
+angles = kinematics.inverse_kinematics(leg, Coords(23.5, 0, 0))
+# leg.set_angle(0, angles[0])
+# leg.set_angle(1, angles[0])
+# leg.set_angle(2, angles[0])
+
+# leg.set_angle(0, 100)  # 0 = elbow
+# leg.set_angle(1, 50)  # 1 = shoulder
+# leg.set_angle(2, 100)  # 2 = base
+
+hexapod.interactive_effector_control(0)
