d5ae213ff85c6400b4188ee16ae07e901cae72f4
valgrut <naruto987@seznam.cz>
2023-12-21 01:30:44 +0100

    Renamed classes

---
+++ Hexapod-markwtech/src/gate_engine.py
@@ -0,0 +1,16 @@
+
+
+class GateEngine:
+    def __init__(self):
+        # TODO: init gyroskop sensor
+        self.gyroscope = None
+        pass
+
+    def ripple_mode(self):
+        pass
+
+    def wave_mode(self):
+        pass
+
+    def tripod_mode(self):
+        pass
